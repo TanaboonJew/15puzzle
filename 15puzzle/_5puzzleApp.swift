4de1568de7accf3402982bb1806baa937708a435
Tanaboon Jewriyavetch <Tanaboon.jew@gmail.com>
2024-02-14 12:58:24 +0700

    Initial Commit

---
+++ 15puzzle/_5puzzleApp.swift
@@ -0,0 +1,17 @@
+//
+//  _5puzzleApp.swift
+//  15puzzle
+//
+//  Created by นายธนบูรณ์ จิวริยเวชช์ on 14/2/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct _5puzzleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
